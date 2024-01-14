c38b72189f2389d34553c046fa5858cd89e7f106
Gabriel Ramos <gabrielrammus42@gmail.com>
2024-01-14 01:11:44 -0300

    Create section the plans

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 document.addEventListener('DOMContentLoaded', function() {
-    const buttons = document.querySelectorAll('[data-tab-button');
+    const buttons = document.querySelectorAll('[data-tab-button]'); //Pegando todos os botões e tendo um array como retorno.
     
-    for (let i = 0; i < buttons.length; i++) {
+    for (let i = 0; i < buttons.length; i++) { //adicionando um evento ao click dos botões
         buttons[i].addEventListener('click', function(botao) {
             const abaAlvo = botao.target.dataset.tabButton;
             const aba = document.querySelector(`[data-tab-id=${abaAlvo}]`);
@@ -14,7 +14,7 @@ document.addEventListener('DOMContentLoaded', function() {
 })
 
 function removeBotaoAtivo() {
-    const buttons = document.querySelectorAll('[data-tab-button');
+    const buttons = document.querySelectorAll('[data-tab-button]');
 
     for (let i = 0; i < buttons.length; i++) {
         buttons[i].classList.remove('shows__tabs__button--is-active');
@@ -22,7 +22,7 @@ function removeBotaoAtivo() {
 }
 
 function escondeTodasAbas() {
-    const tabsContainer = document.querySelectorAll('[data-tab-id');
+    const tabsContainer = document.querySelectorAll('[data-tab-id]');
 
     for (let i = 0; i < tabsContainer.length; i++) {
         tabsContainer[i].classList.remove('shows__list--is-active');
